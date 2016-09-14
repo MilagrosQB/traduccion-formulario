c57e41dcac8ac23a5e8a5bb7ef818e3da33b5649
MilagrosQB <qbmilagros@gmail.com>
2016-09-14 14:06:32 -0500

    traducido a español

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+document.getElementById("form-signin-heading").textContent = "Por favor inicia sesión";
+document.querySelector("span").innerHTML = "Recordar datos";
+document.querySelector("button").innerHTML = "Iniciar Sesión";
 
+var inputEmail = document.getElementById ("inputEmail");
+var inputPassword = document.getElementById ("inputPassword");
+inputEmail.placeholder = "Correo Electrónico";
+inputPassword.placeholder = "Contraseña";
